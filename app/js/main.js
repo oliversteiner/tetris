57b645913d973b466d1fc2e4cde8232f78426741
Oliver Steiner <oliver@mollo.ch>
2017-09-01 14:01:58 +0200

    js test läuft

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,31 @@ var elem_test=document.getElementById('js-main-test');
 console.log(elem_test);
 elem_test.style.backgroundColor = "green";
 elem_test.style.color = "white";
+
+
+
+function figurVonOben(){
+
+  /*
+  * figur
+  * end-zeile
+  * spalte
+  *
+  *
+  *
+  * */
+
+  const figur = document.getElementById('figur-test');
+
+  figur.classList.add('position-ende-zeile-3');
+
+
+return true;
+}
+
+function reset() {
+  const figur = document.getElementById('figur-test');
+
+  figur.classList.remove('position-ende-zeile-3')
+
+}

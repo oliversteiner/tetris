84a52c63174bf09d69334dba9decf1522a4ae4cb
mkegger <michaelegger@macbookpro-maikl.local>
2017-09-01 12:40:57 +0200

    js verknüpfung test ok

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
 console.log('main.js');
+var elem_test=document.getElementById('js-main-test');
+console.log(elem_test);
+elem_test.style.backgroundColor = "green";
+elem_test.style.color = "white";
